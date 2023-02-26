6c698ee4f88ec5b8b9994ab6a9c930f16c8285d7
dnachman <david@nachman.org>
2023-02-26 22:12:36 +0000

    done with honeycomb lesson

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -6,8 +6,13 @@ tracer = trace.get_tracer("home.activities")
 class HomeActivities:
   def run():
     with tracer.start_as_current_span("home-activities-mock-data"):
-
+      span = trace.get_current_span()
+      
       now = datetime.now(timezone.utc).astimezone()
+      
+      # pass value to span
+      span.set_attribute("app.new", now.isoformat())
+
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -47,4 +52,5 @@ class HomeActivities:
         'replies': []
       }
       ]
-    return results
+      span.set_attribute("app.result_length", len(results))
+      return results
--- journal/week2.md
+++ journal/week2.md
@@ -2,7 +2,10 @@
 
 ## Set up honeycomb
 - Installed honeycomb.io / open telemetry and honeycomb started to get data:
-![](assets/wk2/honeycomb-query.png)
-![](assets/wk2/honeycomb-dataset.png)
+![Query](assets/wk2/honeycomb-query.png)
+![Dataset](assets/wk2/honeycomb-dataset.png)
 - Created a manual span for mock-data and get it in trace:
-![](assets/wk2/honeycomb-trace-mock-data.png)
+![Trace mock data](assets/wk2/honeycomb-trace-mock-data.png)
+- Created some attributes in the span and ran query for it
+![Honeycomb query](assets/wk2/hc-query.png)
+![Honeycomb attributes](assets/wk2/hc-attributes.png)
