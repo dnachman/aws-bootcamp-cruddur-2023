4e4304cd61310b916282a3ae4a7dc0d8f7d1633e
dnachman <david@nachman.org>
2023-02-28 20:56:07 +0000

    adding cloudwatch

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,7 +26,22 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# watchtower / cloudwatch
+import watchtower
+import logging
+from time import strftime
+
 app = Flask(__name__)
+
+# cloudwatch
+# Configuring Logger to Use CloudWatch
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
+LOGGER.addHandler(console_handler)
+LOGGER.addHandler(cw_handler)
+LOGGER.info("Hello from Cruddur")
 
 # xray
 xray_url = os.getenv("AWS_XRAY_URL")
@@ -55,6 +70,12 @@ cors = CORS(
   allow_headers="content-type,if-modified-since",
   methods="OPTIONS,GET,HEAD,POST"
 )
+
+@app.after_request
+def after_request(response):
+  timestamp = strftime('[%Y-%b-%d %H:%M]')
+  LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
+  return response
 
 @app.route("/api/message_groups", methods=['GET'])
 def data_message_groups():
@@ -93,6 +114,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
+  LOGGER.info('Hello Cloudwatch! from /api/activities/home')
   data = HomeActivities.run()
   return data, 200
 
